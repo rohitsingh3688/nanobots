948e395c56fe81330195d7fd8f373d8af486a00d
rohitsingh3688 <rohit.singh16infy@gmail.com>
2020-09-06 11:20:35 +0100

    Update sample_echo.js
    updated

--- botkit-bot/features/sample_echo.js
+++ botkit-bot/features/sample_echo.js
@@ -17,8 +17,23 @@ module.exports = function(controller) {
     controller.hears(['hi','hello','howdy','hey','aloha','hola','bonjour','oi'],['message'], async (bot,message) => {
 
       // do something to respond to message
-      await bot.reply(message,'Oh hai! , Please enter your emp id to continue');
+      await bot.reply(message,'Oh Hi! , Please enter your emp id or type add to continue');
 
     });
+
+    controller.hears(['add','add alloc','add allocation'],['message'], async (bot,message) => {
+
+        // do something to respond to message
+        await bot.reply(message,'Please provide details as below');
+        await bot.reply(message,'EmployeeId?');
+  
+    });
+
+    controller.hears(['bye','bbyee','by','thanks'],['message'], async (bot,message) => {
+
+        // do something to respond to message
+        await bot.reply(message,'Have a nice day. Hope to see you soon.');
+  
+      });
 
 }
